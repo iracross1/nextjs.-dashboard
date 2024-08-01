aa63d052981b09c7c03b06ee786dfdcf17fd6070
iracross1 <cross.i@hotmail.com>
2024-08-01 18:53:58 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Stockwise.</strong> This is the example for the{" "}
+            <strong>Reggie This is the Stockwise PRO.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
